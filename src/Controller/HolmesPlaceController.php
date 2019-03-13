93c55aca1b1e7a9196a8a3f384dc2b885be30723
Gavin Mellors <gavin.mellors@gmail.com>
2019-03-13 15:47:11 +0200

    Sorted out menu display and linking to css

--- src/Controller/HolmesPlaceController.php
+++ src/Controller/HolmesPlaceController.php
@@ -3,17 +3,33 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+//use Symfony\Component\Routing\Annotation\Route;
 
 class HolmesPlaceController extends AbstractController
 {
-    /**
-     * @Route("/", name="holmes_place")
-     */
+   
     public function index()
     {
         return $this->render('holmes_place/index.html.twig', [
             'controller_name' => 'HolmesPlaceController',
+        ]);
+    }
+    
+    public function SaveFuelEntry(Request $request){
+       
+    }
+    
+    public function FuelEntry(){
+        return $this->render('holmes_place/fuelentry.html.twig', [
+            'action' => 'Capture a New Fuel Entry',
+        ]);
+    }
+    
+    public function NewServer(){
+        return $this->render('holmes_place/serverentry.html.twig', [
+            'action' => 'New Server Entry',
         ]);
     }
 }
